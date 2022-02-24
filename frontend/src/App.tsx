0681815d059403bc5b0d28893e04b55ec4980c2d
jacklynch00 <jrlynch00@gmail.com>
2022-02-24 10:32:59 -0500

    Added navigation and routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, BrowserRouter as Router, Routes, Link } from 'react-router-dom';
 import './App.css';
+
+import Home from './Home/Home';
+import Products from './Products/Products';
 
 function App() {
 
@@ -13,77 +16,44 @@ function App() {
 	};
 
 	return (
-		<div className="grid-container">
-			<head>
+		<Router>
+			<div className="grid-container">
 				<link rel="stylesheet" href="style.css" />
 				<title>Jet Piranha</title>
-			</head>
-			<body>
-				<header className="header">
-					<div className="brand">
-						<button onClick={openMenu}>&#9776;</button>
-						<a href="index.htm">Jet Piranha</a>
-					</div>
-					<div className="header-links">
-						<a href="cart.html">Cart</a>
-						<a href="signin.html">Sign In</a>
-					</div>
-				</header>
-				<aside className="sidebar">
-					<h3>Shopping Categories</h3>
-					<button className="sidebar-close-button" onClick={closeMenu}>x</button>
-					<ul>
-						<li>
-							<a href="index.html">Pants</a>
-						</li>
+				<body>
+					<header className="header">
+						<div className="brand">
+							<button onClick={openMenu}>&#9776;</button>
+							<a href="index.htm">Jet Piranha</a>
+						</div>
+						<div className="header-links">
+							<Link to="/">Home</Link>
+							<Link to="/catalog">Catalog</Link>
+						</div>
+					</header>
+					<aside className="sidebar">
+						<h3>Shopping Categories</h3>
+						<button className="sidebar-close-button" onClick={closeMenu}>x</button>
+						<ul>
+							<li>
+								<a href="index.html">Pants</a>
+							</li>
 
-						<li>
-							<a href="index.html">Shirts</a>
-						</li>
-					</ul>
-				</aside>
-				<main className="main">
-					<div className="content">
-						<ul className="products">
 							<li>
-								<div className="product">
-									<img className="product-image" src="images/d1.jpg" alt="product" />
-									<div className="product-name">
-										<a href="product.html">Slim Shirt</a>
-									</div>
-									<div className="product-brand">Nike</div>
-									<div className="product-price">$60</div>
-									<div className="product-rating">4.5 Stars (10 Reviews)</div>
-								</div>
-							</li>
-							<li>
-								<div className="product">
-									<img className="product-image" src="images/d1.jpg" alt="product" />
-									<div className="product-name">
-										<a href="product.html">Slim Shirt</a>
-									</div>
-									<div className="product-brand">Nike</div>
-									<div className="product-price">$60</div>
-									<div className="product-rating">4.5 Stars (10 Reviews)</div>
-								</div>
-							</li>
-							<li>
-								<div className="product">
-									<img className="product-image" src="images/d1.jpg" alt="product" />
-									<div className="product-name">
-										<a href="product.html">Slim Shirt</a>
-									</div>
-									<div className="product-brand">Nike</div>
-									<div className="product-price">$60</div>
-									<div className="product-rating">4.5 Stars (10 Reviews)</div>
-								</div>
+								<a href="index.html">Shirts</a>
 							</li>
 						</ul>
-					</div>
-				</main>
-				<footer className="footer">&copy; 2021 Jet Piranha</footer>
-			</body>
-		</div>
+					</aside>
+					<main className="main">
+						<Routes>
+							<Route path="/" element={<Home />} />
+							<Route path="/catalog" element={<Products />} />
+						</Routes>
+					</main>
+					<footer className="footer">&copy; 2021 Jet Piranha</footer>
+				</body>
+			</div>
+		</Router>
 	);
 }
 
